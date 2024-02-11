92c502b1630c35d604efb5467b3ea54dcf6b8868
r0227n <developryo@gmail.com>
2024-02-11 19:39:44 +0900

    add create notion_sdk_dart project

---
+++ notion_sdk_dart/example/notion_sdk_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:notion_sdk_dart/notion_sdk_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ notion_sdk_dart/test/notion_sdk_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:notion_sdk_dart/notion_sdk_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
